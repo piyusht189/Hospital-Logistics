6454dcc4fa7ea3184e6ec3c6a493be8aa2871964
Piyush Talele <piyusht189@users.noreply.github.com>
2022-05-22 15:26:37 +0530

    Update firebase-messaging-sw.js

--- Hospital Logistics Management Website/src/firebase-messaging-sw.js
+++ Hospital Logistics Management Website/src/firebase-messaging-sw.js
@@ -7,14 +7,7 @@ importScripts('https://www.gstatic.com/firebasejs/7.18.0/firebase-messaging.js')
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
-    apiKey: "AIzaSyCAEGsd7VnalEEnNdVI3rdEaWynQkSwUXs",
-    authDomain: "drivecraft-labs.firebaseapp.com",
-    databaseURL: "https://drivecraft-labs.firebaseio.com",
-    projectId: "drivecraft-labs",
-    storageBucket: "drivecraft-labs.appspot.com",
-    messagingSenderId: "885290379388",
-    appId: "1:885290379388:web:ec33fdc024db931ae981f9",
-    measurementId: "G-8R7ZE769Q5"
+    details here
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
